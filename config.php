6851cce3631ee35fdddf2aa7434d63598d38a076
Celik Bekir Ebu <ece@marzek.at>
2020-12-15 14:13:54 +0100

    Commit with dev credentials.

--- config.php
+++ config.php
@@ -11,8 +11,8 @@ define('MYSQL_DATABASE', 'axdev');
 */
 
 define('MYSQL_HOST', 'atw1sqld01');
-define('MYSQL_USER', 'marzekeu\sv_axap');
+define('MYSQL_USER', 'sv_axap_local');
 define('MYSQL_PASS', 'MarAx20!');
-define('MYSQL_DATABASE', 'axprod');
+define('MYSQL_DATABASE', 'axdev');
 
 ?>
