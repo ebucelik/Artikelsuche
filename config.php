950aa9075d76f32ed0911639218ced410a8c6891
Celik Bekir Ebu <ece@marzek.at>
2020-12-21 10:01:46 +0100

    Dev DB enabled.

--- config.php
+++ config.php
@@ -3,16 +3,17 @@ error_reporting(E_ALL);
 
 /*
 DEV:
-
+*/
 define('MYSQL_HOST', 'atw1sqld01');
 define('MYSQL_USER', 'sv_axap_local');
 define('MYSQL_PASS', 'MarAx20!');
 define('MYSQL_DATABASE', 'axdev');
-*/
 
+
+/*
 define('MYSQL_HOST', 'atw1sqln01');
 define('MYSQL_USER', 'sv_axap');
 define('MYSQL_PASS', 'MarAx20!');
 define('MYSQL_DATABASE', 'axprod');
-
+*/
 ?>
